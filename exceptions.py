5d4ea023f15c9fdd16d35113d36a389eb74c0829
André Verweij <51133@gmail.com>
2025-02-27 16:04:03 +0100

    allow passing data to errors

--- exceptions.py
+++ exceptions.py
@@ -1,20 +1,35 @@
 class CustomError(Exception):
-    def __init__(self,message):
+    def __init__(self, message, data=None):
+        if data is None:
+            data = {}
+        print(F"error: `{message}` with data: `{data}`")
         self.message = message
         self.generic = ""
 class UserExists(Exception):
-    def __init__(self,message):
+    def __init__(self, message, data=None):
+        if data is None:
+            data = {}
+        print(F"error: `{message}` with data: `{data}`")
         self.message = message
         self.generic = "user already exists"
 class UserMissing(Exception):
-    def __init__(self,message):
+    def __init__(self, message, data=None):
+        if data is None:
+            data = {}
+        print(F"error: `{message}` with data: `{data}`")
         self.message = message
         self.generic = "user does not exist"
 class PasswordRequirementError(Exception):
-    def __init__(self,message):
+    def __init__(self, message, data=None):
+        if data is None:
+            data = {}
+        print(F"error: `{message}` with data: `{data}`")
         self.message = message
         self.generic = "your password was not strong enough"
 class MultipleUsersError(Exception):
-    def __init__(self,message):
+    def __init__(self, message, data=None):
+        if data is None:
+            data = {}
+        print(F"error: `{message}` with data: `{data}`")
         self.message = message
         self.generic = "multiple users exist"
