a7292e1326096dc5c329d4b1e95330fa299d9899
André Verweij <51133@gmail.com>
2025-02-27 16:07:18 +0100

    asked chatgpt for helpt

--- dbapi.py
+++ dbapi.py
@@ -20,43 +20,46 @@ def connection():
     return db, cursor
 
 def update_daemons():
-    db,cursor = dbapi.connection()
+    """Update daemon entries in MySQL."""
+    db, cursor = connection()
     daemons = mcsapi.get_daemon_data()["data"]["remote"]
+
     for daemon in daemons:
         uuid = daemon['uuid']
         ip = daemon['ip']
-        if ip == "localhost": ip = "verweij.site"
-        sql = "SELECT * FROM `daemons` WHERE UUID = %s"
-        params = [uuid]
-        cursor.execute(sql,params)
+        if ip == "localhost":
+            ip = "verweij.site"
+
+        cursor.execute("SELECT * FROM `daemons` WHERE UUID = %s", [uuid])
         found = cursor.fetchall()
-        if len(found) == 1:
-            sql = "UPDATE `daemons` SET IP = %s WHERE UUID = %s"
-            params = [ip,uuid]
-            cursor.execute(sql,params)
+
+        if found:
+            cursor.execute("UPDATE `daemons` SET IP = %s WHERE UUID = %s", [ip, uuid])
         else:
-            sql = "INSERT INTO `daemons` (UUID,IP) VALUES (%s,%s)"
-            params = [uuid,ip]
-            cursor.execute(sql,params)
+            cursor.execute("INSERT INTO `daemons` (UUID, IP) VALUES (%s, %s)", [uuid, ip])
+
     db.commit()
+    cursor.close()
+    db.close()
 
 def update_users():
-    db,cursor = dbapi.connection()
-    users = mcsapi.get_users(1,100)["data"]["data"]
+    """Update user entries in MySQL."""
+    db, cursor = connection()
+    users = mcsapi.get_users(1, 100)["data"]["data"]
+
     for user in users:
         uuid = user['uuid']
         name = user['userName']
         perms = user['permission']
-        sql = "SELECT `UUID` FROM `users` WHERE UUID = %s"
-        params = [uuid]
-        cursor.execute(sql, params)
+
+        cursor.execute("SELECT `UUID` FROM `users` WHERE UUID = %s", [uuid])
         found = cursor.fetchall()
-        if len(found) == 1:
-            sql = "UPDATE `users` SET PERMS = %s WHERE UUID = %s"
-            params = [perms, uuid]
-            cursor.execute(sql, params)
+
+        if found:
+            cursor.execute("UPDATE `users` SET PERMS = %s WHERE UUID = %s", [perms, uuid])
         else:
-            sql = "INSERT INTO `users` (UUID,NAME,PERMS) VALUES (%s,%s,%s)"
-            params = [uuid, name, perms]
-            cursor.execute(sql, params)
+            cursor.execute("INSERT INTO `users` (UUID, NAME, PERMS) VALUES (%s, %s, %s)", [uuid, name, perms])
+
     db.commit()
+    cursor.close()
+    db.close()
